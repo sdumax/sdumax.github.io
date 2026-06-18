36e5180fe4d8ec675a73d581885ebf9fdb13f963
sdumax <maxdiogu@gmail.com>
2026-06-18 19:34:13 +0100

    chore: dismiss jekyll with no.jekyll file

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "docs",
-  basePath: "/sdumax.github.io",
+  basePath: "/",
   images: {
     unoptimized: true,
   },
