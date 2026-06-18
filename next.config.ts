3ba92e891ecd0d4ecd7225061ce3866adc846ed6
sdumax <maxdiogu@gmail.com>
2026-06-18 18:46:30 +0100

    chore: correct assets export path

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "docs",
+  basePath: "/docs",
   images: {
     unoptimized: true,
   },
+  
 };
 
 export default nextConfig;
