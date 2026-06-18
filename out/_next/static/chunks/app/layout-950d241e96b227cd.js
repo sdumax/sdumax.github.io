7acf97d4f33887876e9dc54315d823776cd51f22
sdumax <maxdiogu@gmail.com>
2026-06-18 18:21:11 +0100

    Add initial portfolio files including HTML structure and images
    - Created index.html for the personal portfolio with sections for About, Resume, and Portfolio.
    - Added images for the Tutor Konnect project.
    - Included a text file for additional metadata related to the portfolio.

---
+++ out/_next/static/chunks/app/layout-950d241e96b227cd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2189:()=>{},7672:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2189,23))}},e=>{e.O(0,[630,480,920,358],()=>e(e.s=7672)),_N_E=e.O()}]);
